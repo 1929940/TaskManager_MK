3686b3dc1f813436f66931243761a3b76231dd11
1929940 <1929940@gmail.com>
2019-06-08 23:54:51 +0200

    Logic is Done

--- TaskManager/Windows/DescriptionHelpers.cs
+++ TaskManager/Windows/DescriptionHelpers.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace TaskManager.Windows
 {
     public static class DescriptionHelpers
     {
+        // Method used by both Add and Edit Window
+        // Contains the logic for the word counter located on gui under the textboxes
+
         public static void ValidateTextbox(TextBox textBox, Label label)
         {
             int counter = 0;
@@ -29,7 +28,5 @@ namespace TaskManager.Windows
             }
             label.Content = counter;
         }
-
-
     }
 }
